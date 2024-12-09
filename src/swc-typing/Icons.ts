26e3a2c42323b7da420c9e467324bd19c36a95de
Jian Liao <jianliao@adobe.com>
2024-12-09 10:38:09 -0800

    Improving the type of workflow icon

--- src/swc-typing/Icons.ts
+++ src/swc-typing/Icons.ts
@@ -1,7 +1,8 @@
 import { FC } from "react";
-import { LightIcon as SpLightIcon } from "@spectrum-web-components/icons-workflow";
+import type { LightIcon as LightIconTag } from "@spectrum-web-components/icons-workflow";
+import { IconLight } from "@spectrum-web-components/icons-workflow/src/elements/IconLight.d.ts";
 import "@spectrum-web-components/icons-workflow/icons/sp-icon-light.js";
 
 const SWC_NAME = "sp-icon-light";
 
-export const LightIcon = SWC_NAME as unknown as FC<typeof SpLightIcon>;
+export const LightIcon = SWC_NAME as unknown as FC<typeof LightIconTag | Partial<IconLight>>;
